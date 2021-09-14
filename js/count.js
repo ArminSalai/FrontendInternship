df0614c5f5062016106250e87ff75bf93648015c
ArminSalai <kuka.kari12@gmail.com>
2021-09-14 15:51:33 +0200

    Added several pages, the products page has perfect responsivity, the other pages mostly do not, started the checkout page

---
+++ js/count.js
@@ -0,0 +1,27 @@
+let count = parseInt(document.cookie.slice(";")[0]);
+if(typeof count != "number") count = 0;
+document.querySelector("#counter").innerHTML = count;
+let minus = document.querySelector("#minus");
+let plus = document.querySelector("#plus");
+let countNum = document.querySelector("#countNum");
+countNum.innerHTML = count;
+
+minus.addEventListener('click', function() {
+    if(count > 0)
+    {
+        count -= 1;
+        countNum.innerHTML = count;
+    }
+});
+
+plus.addEventListener('click', function() {
+    count += 1;
+    countNum.innerHTML = count;
+});
+
+let addIt = document.querySelector("#addIt");
+
+addIt.addEventListener('click', function() {
+    document.querySelector("#counter").innerHTML = count;
+    document.cookie = count + "; path=/";
+});
